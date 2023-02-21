39912f868c3eae15cfc337b7318be96e33bafba8
Jakub Krauz <jmkrauz@gmail.com>
2023-02-21 16:29:34 +0100

    PURGE

--- api_server/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod app;
-
-#[cfg(feature = "only_echo")]
-pub mod dummy_server;
-
-#[cfg(not(feature = "only_echo"))]
-pub mod server;
--- database/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod aggregates;
-pub mod client;
-pub mod retrying_client;
-pub mod time_range;
-pub mod user_profiles;
-pub mod user_tag;
